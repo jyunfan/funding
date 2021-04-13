7f730168ee9cc95ade4c905fdab444e98ac791d2
JyunFan Tsai <jyunfan@gmail.com>
2021-04-13 22:34:02 +0800

    Update dydx v1

--- src/python/dydx.py
+++ src/python/dydx.py
@@ -1,3 +1,4 @@
+import argparse
 import datetime
 import os
 import json
@@ -13,14 +14,22 @@ def convert2csv(data):
                 fid.write(','.join(record.keys()) + '\n')
                 fid.write(','.join(record.values()) + '\n')
 
+def fetch(last_date):
+    if last_date:
+        r = requests.get(f'https://api.dydx.exchange/v1/historical-funding-rates?startingBefore={last_date}')
+    else:
+        r = requests.get(f'https://api.dydx.exchange/v1/historical-funding-rates')
+    result = r.json()
+    print("pair count:", len(result))
+    return result
+
+
 if __name__ == '__main__':
-    while True:
-        print('Download fuding rate from dydx')
-        r = requests.get('https://api.dydx.exchange/v1/historical-funding-rates')
-        result = r.json()
-        print("pair count:", len(result))
-        curtime = datetime.datetime.now()
-        convert2csv(result)
-        print('Sleep')
-        time.sleep(60*50)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--last_date", default='')
+    args = parser.parse_args()
+
+    result = fetch(args.last_date)
+    convert2csv(result)
+
 
